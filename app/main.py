517d9a06a097b98796ee83a38737b423b8ef4df8
Derek9745 <31524195+Derek9745@users.noreply.github.com>
2026-04-04 20:25:43 -0400

    database html rendering fixed

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-#main file
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Templates
+templates = Jinja2Templates(directory="templates")
+
+# Mount the static folder at /static
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def get(request: Request):
+    return templates.TemplateResponse(request = request,name="index.html")
